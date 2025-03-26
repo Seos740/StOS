3b0df5292cd59301e84da0d876b09ac64307f423
Seos740 <dicksonstanley503@gmail.com>
2025-03-26 18:20:16 +0800

    Add files via upload

--- Kernel/Code/KernelStart.cpp
+++ Kernel/Code/KernelStart.cpp
@@ -1,4 +1,21 @@
-extern "C" void main(){
-    *(char*)0xb8000 = 'Q';
-    return;
+#include "KeyboardRead.h"
+
+extern "C" void clear_screen() {
+    char* video_memory = (char*)0xB8000;
+    for (int i = 0; i < 80 * 25 * 2; i += 2) {
+        video_memory[i] = ' ';      
+        video_memory[i + 1] = 0x07; 
+    }
+}
+
+extern "C" void kernel_main() {
+    char* video_memory = (char*)0xB8000;
+    const char* message = "StanleyOS Kernel is running in 32-Bit Mode. put your own code for once";
+    
+    for (int i = 0; message[i] != '\0'; i++) {
+        video_memory[i * 2] = message[i];   
+        video_memory[i * 2 + 1] = 0x07;     
+    }
+
+    while (1);
 }
---
+++ Kernel/Code/KeyboardRead.h
@@ -0,0 +1,9 @@
+// KeyboardRead.h
+#ifndef KEYBOARD_READ_H
+#define KEYBOARD_READ_H
+
+#include <stdint.h>
+
+uint8_t read_scan_code();
+
+#endif
